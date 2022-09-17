51ac1fbfddf156d41cf5414c7ed9a804269ea501
ZaniarJavanroodi <113790351+ZaniarJavanroodi@users.noreply.github.com>
2022-09-17 21:14:41 +0430

    Add files via upload

---
+++ Assignment1/src/UserPackage/User.java
@@ -0,0 +1,198 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserPackage;
+
+
+/**
+ *
+ * @author Zaniar
+ */
+public class User {
+    private String FirstName;
+    private String LastName;
+    private String DOB;
+    private int Age;
+    private Long TelephoneNumber;
+    private String Email;
+    private String StreetLine1;
+    private String StreetLine2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Degree1;
+    private String Degree1S;
+    private String Degree1E;
+    private String Degree2;
+    private String Degree2S;
+    private String Degree2E;
+    private String Photo;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public Long getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(Long TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getStreetLine1() {
+        return StreetLine1;
+    }
+
+    public void setStreetLine1(String StreetLine1) {
+        this.StreetLine1 = StreetLine1;
+    }
+
+    public String getStreetLine2() {
+        return StreetLine2;
+    }
+
+    public void setStreetLine2(String StreetLine2) {
+        this.StreetLine2 = StreetLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public String getDegree1() {
+        return Degree1;
+    }
+
+    public void setDegree1(String Degree1) {
+        this.Degree1 = Degree1;
+    }
+
+    public String getDegree1S() {
+        return Degree1S;
+    }
+
+    public void setDegree1S(String Degree1S) {
+        this.Degree1S = Degree1S;
+    }
+
+    public String getDegree1E() {
+        return Degree1E;
+    }
+
+    public void setDegree1E(String Degree1E) {
+        this.Degree1E = Degree1E;
+    }
+
+    public String getDegree2() {
+        return Degree2;
+    }
+
+    public void setDegree2(String Degree2) {
+        this.Degree2 = Degree2;
+    }
+
+    public String getDegree2S() {
+        return Degree2S;
+    }
+
+    public void setDegree2S(String Degree2S) {
+        this.Degree2S = Degree2S;
+    }
+
+    public String getDegree2E() {
+        return Degree2E;
+    }
+
+    public void setDegree2E(String Degree2E) {
+        this.Degree2E = Degree2E;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+    
+    
+    
+  
+
+}
